18a91ea644a439cb173ff62cbd88d3eb78a07427
VeljkoDencic1 <vdencic@griddynamics.com>
2022-08-18 11:24:30 +0200

    prvi

---
+++ index.php
@@ -0,0 +1,59 @@
+<!-- 
+#
+// #$connect = mysqli_connect(
+//     'db',
+//     'lamp_docker',
+//     'password',
+//     'lamp_docker'
+// );
+
+// $query = 'SELECT * FROM blog';
+// $result = mysqli_query($connect,$query);
+
+// echo '<h1>MySQL Content:</h1>';
+
+// while($record = mysqli_fetch_assoc($result)){
+
+//     echo '<h2>'.$record['Title'].'</h2>';
+//     echo '<h2>'.$record['Content'].'</h2>';
+//     echo 'Posted: '.$record['Date'];
+//     echo '<hr>';
+
+// } -->
+
+<html>
+    <head>
+        <title>Welcome to LAMP Infrastructure</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+                echo "<h1>Podaci iz baze:</h1>";
+
+                $conn = mysqli_connect('db', 'root', 'test', "dbname");
+
+                $query = 'SELECT * From Person';
+                $result = mysqli_query($conn, $query);
+
+                echo '<table class="table table-striped">';
+                echo '<thead><tr><th></th><th>id</th><th>name</th></tr></thead>';
+                while($value = $result->fetch_array(MYSQLI_ASSOC)){
+                    echo '<tr>';
+                    echo '<td><a href="#"><span class="glyphicon glyphicon-search"></span></a></td>';
+                    foreach($value as $element){
+                        echo '<td>' . $element . '</td>';
+                    }
+
+                    echo '</tr>';
+                }
+                echo '</table>';
+
+                $result->close();
+                mysqli_close($conn);
+            ?>
+        </div>
+    </body>
+</html>
